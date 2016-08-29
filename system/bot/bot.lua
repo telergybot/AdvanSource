2283941d7eabe109777b45cf8319919752270dcb
amirreza <negahapp@chmail.ir>
2016-08-29 15:18:28 +0430

    Update bot.lua

--- system/bot/bot.lua
+++ system/bot/bot.lua
@@ -244,7 +244,7 @@ function create_config( )
     "whitelist"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {253364622},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'system/data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
